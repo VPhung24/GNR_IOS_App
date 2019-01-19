ef7428ed34868ab5ff40361081ca4cbd0385b355
Vivian Phung <VPhung11@gmail.com>
2019-01-18 21:31:02 -0500

    Initial Commit

---
+++ GNR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GNR
+//
+//  Created by Vivian Phung on 1/18/19.
+//  Copyright © 2019 Vivian Phung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
